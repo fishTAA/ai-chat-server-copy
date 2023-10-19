116ee544d058eb59173e5cf32d98eb8abad21b35
Tom Anthony Aguila <tom31aguila@gmail.com>
2023-10-19 12:03:29 -0700

    [fix] db connection
    the connection request will run only once.

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,16 +2,25 @@ import { Db, MongoClient } from "mongodb";
 
 const connectionString = process.env.ATLAS_URI || "";
 
-export const getConnection = async (): Promise<Db> => {
-  const client = new MongoClient(connectionString);
+
+const client = new MongoClient(connectionString);
   
-  let conn: MongoClient;
+let conn: MongoClient;
+const makeConnection = async () => {
   try {
-    conn = await client.connect();
+    await client.connect().then(con => conn = con);
+    console.log("Connected to DB")
   } catch(e) {
     console.error(e);
+  }  
+
+}
+
+
+export const getConnection = async (): Promise<Db> => {
+  if (!conn) {
+    await makeConnection();
   }
-  
   const dbConnection: Db = conn.db("IT-Support");
   return dbConnection;
 }

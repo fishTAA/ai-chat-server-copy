7f2f798792a739d0451e37262c9768d88b0ea78d
Tom Anthony Aguila <tom31aguila@gmail.com>
2024-01-22 06:23:48 -0800

    [add] get embedding collection

--- src/router/embedding.ts
+++ src/router/embedding.ts
@@ -2,10 +2,12 @@ import express from "express";
 
 import { newEmbedding, testEmbedding, searchDocument } from "../controllers";
 import { isAdmin, isAuthenticated } from "../middlewares";
+import { getEmbeddingCollection } from "../ai/embeddings";
 
 export default (router: express.Router) => {
   router.post("/createEmbedding", isAuthenticated, isAdmin, newEmbedding);
   router.post("/createHtmlEmbedding", isAuthenticated, isAdmin, newEmbedding);
   router.post("/testEmbedding", isAuthenticated, testEmbedding);
   router.get("/findDocument", isAuthenticated, searchDocument);
+  router.get("/embeddingCollection",isAuthenticated,isAdmin, getEmbeddingCollection);
 };
